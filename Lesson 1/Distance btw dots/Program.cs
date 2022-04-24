df3ca7172ee49506c70719b66a862dc354955076
Yaroslav Rezchikov <rezchikov.md@gmail.com>
2022-04-24 19:24:13 +0400

    feat: Upload Lesson 1

---
+++ Lesson 1/Distance btw dots/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceBtwDots
+{
+    /*
+        а) Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2 по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2). 
+           Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой);
+        б) Выполнить предыдущее задание, оформив вычисления расстояния между точками в виде метода.
+     */
+
+    //Резчиков Ярослав
+    internal class DistanceBtwDots
+    {
+
+        static void Main(string[] args)
+        {
+            // User Input
+
+            Console.Write("X1 = ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("Y1 = ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("X2 = ");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.Write("Y1 = ");
+            int y2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetDistanceBtwDots(x1, y1, x2, y2)); 
+
+            // Вычисление distanceBtwDots в Main
+            /*
+            double distanceBtwDots =Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+            Console.WriteLine(distanceBtwDots.ToString("0.##"));
+            */
+        }
+
+        static string GetDistanceBtwDots(int x1,int y1, int x2, int y2)
+        {
+            double distanceBtwDots = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+            return distanceBtwDots.ToString("0.##");
+        }
+    }
+}
---
+++ Lesson 1/Print To Screen Center/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_To_Screen_Center
+{
+    internal class Program
+    {
+        /*
+        а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+        б) Сделать задание, только вывод организовать в центре экрана.
+        в) *Сделать задание б с использованием собственных методов (например, Print(string ms, int x,int y).
+         */
+
+        //Резчиков Ярослав
+        static void Main(string[] args)
+        {
+            string[] dataToPrint = new string[] { "Yaroslav", "Rezchikov", "Erevan" };
+            for (int i = 0; i < dataToPrint.Length; i++)
+            {
+            Print(dataToPrint[i], (Console.WindowWidth / 2 - (dataToPrint[i].Length/2)), Console.WindowHeight / 2+i);
+            }
+        }
+        static void Print(string output, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(output);
+        }
+    }
+}
