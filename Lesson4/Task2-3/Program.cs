e9c03ff90086373c912a12ec24f06f503efd134f
Yaroslav Rezchikov <rezchikov.md@gmail.com>
2022-05-13 12:16:58 +0300

    Add 4 Lesson, Update Yar.Learning

---
+++ Lesson4/Task2-3/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Yar.Learning;
+using static System.Console;
+
+namespace Task2_3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            OutputSamples.PrintHead(4);
+            WriteLine("Продемонстрировать работу библиотеки с классом MyArray");
+            
+            ForegroundColor = ConsoleColor.DarkGreen;
+            WriteLine("1. Creating object from an array...");
+            ResetColor();
+            MyArray fromArray = new MyArray(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            fromArray.PrintMyArray();
+            ReadKey();
+            
+            ForegroundColor = ConsoleColor.DarkGreen;
+            WriteLine("2. Creating object from Input.txt. Feel free to change it, or it's name...");
+            ResetColor();
+            string fileName = AppDomain.CurrentDomain.BaseDirectory + "Input.txt";
+            bool flagForFile = true;
+            try
+            {
+                MyArray catcher = new MyArray(fileName);
+            }
+            catch (Exception e)
+            {
+                WriteLine("File Not Found");
+                flagForFile = false;
+            }
+            if (flagForFile)
+            {
+                MyArray fromInput = new MyArray(fileName);
+                fromInput.PrintMyArray();
+            }
+            ReadKey();
+            
+            ForegroundColor = ConsoleColor.DarkGreen;
+            WriteLine("3. Creating an object with given parameters...");
+            ResetColor();
+            ReadKey();
+            
+            WriteLine("This will require:");
+            WriteLine("Length (natural positive number, not zero): ");
+            int length = UserInputNaturalPositiveNotZero();
+            WriteLine("First value for an object: ");
+            int firstValue = UserInputNatural();
+            WriteLine("And a step: ");
+            int step = UserInputNatural();
+            WriteLine("Final object:");
+            MyArray fromGivenValues = new MyArray(length, firstValue, step);
+            fromGivenValues.PrintMyArray();
+            ReadKey();
+            
+            ForegroundColor = ConsoleColor.DarkGreen;
+            WriteLine("4. Now let's create one with random values...");
+            ResetColor();
+            WriteLine("Length (natural, positive, not zero): ");
+            length = UserInputNaturalPositiveNotZero();
+            WriteLine("Range start: ");
+            int from = UserInputNatural();
+            WriteLine("And an end of range: ");
+            int to = UserInputNatural();
+            MyArray randomValues = new MyArray();
+            randomValues = MyArray.CreateRandomNumberArray(length, from, to);
+            WriteLine("Final object: ");
+            randomValues.PrintMyArray();
+            ReadKey();
+
+            ForegroundColor = ConsoleColor.DarkGreen;
+            Write("5. Now for a randomValue object, let's count all pairs one of the elements in witch is divisible by 3 (Task1): ");
+            ResetColor();
+            WriteLine(MyArray.Task1(randomValues,3));
+            ReadKey();
+            
+            ForegroundColor = ConsoleColor.DarkGreen;
+            Write("6. Sum of all elements: ");
+            ResetColor();
+            WriteLine(randomValues.Sum);
+            ReadKey();
+            
+            ForegroundColor = ConsoleColor.DarkGreen;
+            Write("7. Number of elements with max value: ");
+            ResetColor();
+            WriteLine(randomValues.MaxCount);
+            ReadKey();
+            
+            ForegroundColor = ConsoleColor.DarkGreen;
+            Write("8. Now, let's multiply all elements by ... ");
+            ResetColor();
+            int multiplier = UserInputNatural();
+            randomValues.Multi(multiplier);
+            randomValues.PrintMyArray();
+            ReadKey();
+            
+            ForegroundColor = ConsoleColor.DarkGreen;
+            WriteLine("9. Inverted: ");
+            ResetColor();
+            MyArray inversedRandom = MyArray.Inverse(randomValues);
+            inversedRandom.PrintMyArray();
+            ReadKey();
+            
+            ForegroundColor = ConsoleColor.DarkGreen;
+            WriteLine("10. Number of occurrences for each element in random array object: ");
+            ResetColor();
+            MyArray.NumberOfOccurrences(randomValues);
+            ReadKey();
+        }
+
+        private static int UserInputNaturalPositiveNotZero()
+        {
+            while (true)
+            {
+                if (!(int.TryParse(ReadLine(), out int userInput) && (userInput != 0) && (userInput > 0)))
+                {
+                    WriteLine("Incorrect Input! Try again...");
+                    continue;
+                }
+                return userInput;
+            }
+        }
+        private static int UserInputNatural()
+        {
+            while (true)
+            {
+                if (!(int.TryParse(ReadLine(), out int userInput)))
+                {
+                    WriteLine("Incorrect Input. Try again.");
+                    continue;
+                }
+                return userInput;
+            }
+        }
+    }
+}
---
+++ Lesson4/Task5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.CodeDom;
+using System.IO;
+using Yar.Learning;
+using static System.Console;
+
+namespace Task5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Empty object
+            TwoDimensionalArray twoDimEmpty = new TwoDimensionalArray();
+            
+            //Object with random values
+            TwoDimensionalArray twoDimRandom = new TwoDimensionalArray(5, 5, 10, 50);
+            
+            //Object from two dimensional array
+            int[,] twoDimArr = new int[,] { { 1, 2, 3, 4, 5 }, { 1, 2, 3, 4, 5 } };
+            TwoDimensionalArray twoDimFromArray = new TwoDimensionalArray(twoDimArr);
+            
+            //Object from file
+            TwoDimensionalArray twoDimFromFile = new TwoDimensionalArray(AppDomain.CurrentDomain.BaseDirectory + "Input.txt", 2);
+
+            //Prints object elements via tabulation
+            TwoDimensionalArray.PrintTwoDimArr(twoDimRandom);
+            
+            //Object length - property
+            Write("Length:");
+            WriteLine(twoDimRandom.Length);
+            
+            //Gets length of object dimension
+            Write("Length of 0 dimension: ");
+            WriteLine(twoDimRandom.GetLength(0));
+            
+            //Gets min element value
+            Write("Min element value: ");
+            WriteLine(twoDimRandom.MinElement);
+            
+            //Gets max element value
+            Write("Max element value: ");
+            WriteLine(twoDimRandom.MaxElement);
+            
+            //Sums all elements - property
+            Write("Sum of all elements: ");
+            WriteLine(twoDimRandom.SumAllElements());
+            
+            //Sums all elements which value is greater than border - property
+            Write("Sum of all elements greater than 45: ");
+            WriteLine(twoDimRandom.SumAllElementsGreaterThan(45));
+            
+            //Creates txt file with values of the object
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "Output.txt";
+            twoDimRandom.CreateFile(filePath);
+            
+            //Initializes coordinates of element with max value
+            TwoDimensionalArray.CoordinatesOfMaxElement(twoDimRandom,out int x, out int y);
+            WriteLine($"Max element at: {x},{y}");
+        }
+    }
+}
